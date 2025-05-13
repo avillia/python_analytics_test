0d1e3ab360d7d6a3c62443313505666052de84d9
avillia <avil48job@gmail.com>
2025-05-13 21:23:13 +0300

    feat(auth): create /login endpoint

---
+++ src/api/routes/auth.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel, EmailStr
+
+from src.core.handlers.auth import obtain_jwt_token_for
+
+auth_router = APIRouter(
+    prefix="/auth",
+    tags=["service"],
+)
+
+class UserLogin(BaseModel):
+    login: str
+    password: str
+
+
+class UserSignUp(BaseModel):
+    email: EmailStr
+    login: str
+    password: str
+
+
+@auth_router.get("/")
+async def login(credentials: UserLogin) -> dict:
+    try:
+        return {"access_token": obtain_jwt_token_for(credentials.login, credentials.password), "token_type": "bearer"}
+    except KeyError:
+        raise HTTPException(status_code=401, detail=f"Invalid password for {login=}!")
+    except LookupError:
+        raise HTTPException(status_code=401, detail=f"Invalid username: no user with such {login=} exists!")
