040b9f246f7cb791736b7ea5ef74803da6834a6d
avillia <avil48job@gmail.com>
2025-05-14 11:38:50 +0300

    feat(api): imbue /auth endpoint with user creation and role assigning capabilities

--- src/api/routes/auth.py
+++ src/api/routes/auth.py
@@ -1,7 +1,11 @@
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel, EmailStr
 
-from src.core.handlers.auth import obtain_jwt_token_for
+from src.core.handlers.auth import (
+    assign_existing_role_with,
+    create_new_user_with_following,
+    obtain_jwt_token_for,
+)
 
 auth_router = APIRouter(
     prefix="/auth",
@@ -20,12 +24,18 @@ class UserSignUp(BaseModel):
     password: str
 
 
+class AssignRoleRequest(BaseModel):
+    login: str
+    role_name: str
+
+
 @auth_router.get("/")
 async def login(credentials: UserLogin) -> dict:
     try:
         return {
             "access_token": obtain_jwt_token_for(
-                credentials.login, credentials.password
+                credentials.login,
+                credentials.password,
             ),
             "token_type": "bearer",
         }
@@ -36,3 +46,32 @@ async def login(credentials: UserLogin) -> dict:
             status_code=401,
             detail=f"Invalid username: no user with such {login=} exists!",
         )
+
+
+@auth_router.post("/", status_code=201)
+async def signup(credentials: UserSignUp) -> dict:
+    try:
+        create_new_user_with_following(
+            credentials.login,
+            credentials.email,
+            credentials.password,
+        )
+        return {"status": "success", "login_url": auth_router.url_path_for("login")}
+    except LookupError:
+        raise HTTPException(
+            status_code=401,
+            detail="Looks like these credentials are already occupied! Try different one.",
+        )
+
+
+@auth_router.patch("/")
+async def assign_role(new_role_request: AssignRoleRequest) -> dict:
+    if assign_existing_role_with(new_role_request.role_name, to=new_role_request.login):
+        return {
+            "status": "success",
+            "info": f"User '{new_role_request.login}' is now '{new_role_request.role_name}!",
+        }
+    return {
+        "status": "OK",
+        "info": f"No changes: User '{new_role_request.login}' is already '{new_role_request.role_name}",
+    }
