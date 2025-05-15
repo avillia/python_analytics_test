853668ee796bca7cd028869391c7ba18b301d5f7
avillia <avil48job@gmail.com>
2025-05-15 22:46:56 +0300

    feat(receipts): implement method to convert ORM-model to dict so we can render it as txt

--- src/core/handlers/receipts/get.py
+++ src/core/handlers/receipts/get.py
@@ -3,7 +3,32 @@ from src.core.db.models import Receipt
 from src.core.handlers.receipts.rendering import build_str_repr_of_receipt
 
 
-def convert_to_dict_repr(receipt_raw_data: Receipt) -> dict: ...
+def convert_to_dict_repr(receipt_raw_data: Receipt) -> dict[str, str]:
+    items: list[dict[str, str]] = []
+    for it in receipt_raw_data.items:
+        items.append(
+            {
+                "name": it.name,
+                "price": str(it.price),
+                "quantity": str(it.quantity),
+                "total": str(it.total),
+            }
+        )
+
+    payment_type = "cashless" if receipt_raw_data.is_cashless_payment else "cash"
+    payment = {
+        "type": payment_type,
+        "amount": str(receipt_raw_data.payment_amount),
+    }
+
+    return {
+        "issuer": receipt_raw_data.user.name,
+        "items": items,
+        "total": str(receipt_raw_data.total),
+        "payment": payment,
+        "rest": str(receipt_raw_data.rest),
+        "created_at": receipt_raw_data.creation_date,
+    }
 
 
 def render_as_str_receipt_with(receipt_id: str, width: int) -> str:
