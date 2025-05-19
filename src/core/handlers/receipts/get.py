d085b3331c3302b7bcb35aaf35cf78f5e5df0463
avillia <avil48job@gmail.com>
2025-05-19 16:32:25 +0300

    fix(receipts): make sure we use only one session to avoid imposing joinedload(Items) for performance considerations

--- src/core/handlers/receipts/get.py
+++ src/core/handlers/receipts/get.py
@@ -1,3 +1,4 @@
+from src.core.db.base import create_session
 from src.core.db.managers import DBAppConfigManager, ReceiptCacheManager, ReceiptManager
 from src.core.db.models import Receipt
 from src.core.handlers.receipts.rendering import build_str_repr_of_receipt
@@ -5,13 +6,13 @@ from src.core.handlers.receipts.rendering import build_str_repr_of_receipt
 
 def convert_to_dict_repr(receipt_raw_data: Receipt) -> dict[str, str]:
     items: list[dict[str, str]] = []
-    for it in receipt_raw_data.items:
+    for item in receipt_raw_data.items:
         items.append(
             {
-                "name": it.name,
-                "price": str(it.price),
-                "quantity": str(it.quantity),
-                "total": str(it.total),
+                "name": item.name,
+                "price": str(item.price),
+                "quantity": str(item.quantity),
+                "total": str(item.total),
             }
         )
 
@@ -32,27 +33,31 @@ def convert_to_dict_repr(receipt_raw_data: Receipt) -> dict[str, str]:
 
 
 def render_as_str_receipt_with(receipt_id: str, width: int) -> str:
-    receipt_raw_data: Receipt | None = ReceiptManager().fetch_specific_by(receipt_id)
-    if receipt_raw_data is None:
-        raise KeyError(f"Receipt(id={receipt_id}) is not found in DB!")
+    with create_session() as session:
+        receipt_raw_data: Receipt | None = ReceiptManager(session).fetch_specific_by(
+            receipt_id
+        )
+        if receipt_raw_data is None:
+            raise KeyError(f"Receipt(id={receipt_id}) is not found in DB!")
 
-    formatting_config: dict[str, str | int] = (
-        DBAppConfigManager().fetch_receipt_formatting_configs()
-    )
-    formatting_config["width"] = width
+        formatting_config: dict[str, str | int] = DBAppConfigManager(
+            session
+        ).fetch_receipt_formatting_configs()
 
-    config_string: str = ":".join(formatting_config.values())
+        config_string: str = ":".join(formatting_config.values()) + f":{width}"
 
-    cache = ReceiptCacheManager()
+        cache = ReceiptCacheManager(session)
 
-    cached_txt = cache.fetch_cache_for(receipt_id, config_string)
-    if cached_txt:
-        return cached_txt
+        cached_txt = cache.fetch_cache_for(receipt_id, config_string)
+        if cached_txt:
+            return cached_txt
 
-    rendered_receipt = build_str_repr_of_receipt(
-        convert_to_dict_repr(receipt_raw_data),
-        formatting_config,
-    )
-    cache.create_new_entry_with(receipt_id, config_string, rendered_receipt)
+        formatting_config["width"] = width
+
+        rendered_receipt = build_str_repr_of_receipt(
+            convert_to_dict_repr(receipt_raw_data),
+            formatting_config,
+        )
+        cache.create_new_entry_with(receipt_id, config_string, rendered_receipt)
 
     return rendered_receipt
