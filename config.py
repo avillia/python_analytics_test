609c924418d7abaae781fe97ee89fb677f7204f0
avillia <avil48job@gmail.com>
2025-05-19 22:04:39 +0300

    test(app): make sure we use file-db instead of in-memory one because it's much more convenient

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from os import getenv
 
-DATABASE_URL = getenv("DATABASE_URL", "sqlite:///:memory:")
+DATABASE_URL = getenv("DATABASE_URL", "sqlite:///./test.db")
 CRYPTO_PEPPER = getenv("CRYPTO_PEPPER", "test-secret")
 JWT_SECRET_KEY = getenv("JWT_SECRET_KEY", "test-secret")
 JWT_ALGORITHM = getenv("JWT_ALGORITHM", "HS256")
